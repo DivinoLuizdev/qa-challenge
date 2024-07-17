98ffeea96dda96fabed3ee89dee766c2c7d8a742
Divino luiz <juniorwar360@gmail.com>
2024-07-17 10:19:45 -0300

    Configurações iniciais do projeto: adiciona .gitignore e cria estrutura base do projeto Cypress

---
+++ cypress.config.js
@@ -0,0 +1,12 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl:'https://qastage.buildbox.one/18/cadastro/',
+    viewportWidth: 1920,
+    viewportHeight:1080,
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
